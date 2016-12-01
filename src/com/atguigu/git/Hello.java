81beeb1bd72573da5339d8d792523a81b8c74549
devjjw <jiangjunwen_job@aliyun.com>
2016-12-01 15:58:17 +0800

    haha
    111

--- src/com/atguigu/git/Hello.java
+++ src/com/atguigu/git/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("哈哈");
 		System.err.println("ZZ");
-		System.out.println("HH")
+		System.out.println("HH");
+		System.out.println("沖突");
 	}
 }
